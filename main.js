c6a80460cfc948e214d10bff337642ce087fd1c6
Colin Jaffe <balloonasaurus@gmail.com>
2019-08-06 15:46:25 -0400

    completes readme

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 // Global data.
 // A few todos in there to start with!
 // This is mostly for testing purposes.
-let todos = [
-  `Be able to mark todos "done".`,
-  `Allow user interaction through the DOM`,
-  `Add dates to todos.`,
-];
+// let todos = [
+//   `Be able to mark todos "done".`,
+//   `Allow user interaction through the DOM`,
+//   `Add dates to todos.`,
+// ];
 
-let completed = [
-  false,
-  false,
-  false,
-]
+// let completed = [
+//   false,
+//   false,
+//   false,
+// ]
 
 // Or the ALTERNATE method:
-// let todos = [
-//   [`Be able to mark todos "done".`, false],
-//   [`Allow user interaction through the DOM`, false],
-//   [`Add dates to todos.`, false],
-// ]
+let todos = [
+  [`Be able to mark todos "done".`, false],
+  [`Allow user interaction through the DOM`, false],
+  [`Add dates to todos.`, false],
+]
+
